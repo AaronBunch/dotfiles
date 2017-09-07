03bdb7a42a27b8f65727f31e0c1d3e25e2d0d184
Aaron Bunch <aaronbunch713@gmail.com>
2017-09-07 08:37:40 -0700

    added bashrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile vimrc"    	  # list of files/folders to symlink in homedir
+dir=~/dotfiles                            # dotfiles directory
+olddir=~/dotfiles_old                     # old dotfiles backup directory
+files="bash_profile bashrc vimrc"    	  # list of files/folders to symlink in homedir
 
 ##########
 
